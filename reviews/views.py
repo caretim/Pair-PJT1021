ccb111c299f85cfa00bde15a3fd33dba28b3c8e6
psun0610 <psy010610@naver.com>
2022-10-21 13:09:47 +0900

    function reviews-detail

--- reviews/views.py
+++ reviews/views.py
@@ -20,11 +20,18 @@ def create(request):
     }
     return render(request, 'reviews/create.html', context)
 
+
 def index(request):
     all_data = Review.objects.order_by('-pk')
-
     context = {
         'all_data' : all_data,
     }
+    return render(request, 'reviews/index.html', context)
 
-    return render( request, 'reviews/index.html', context)
+
+def detail(request, review_pk):
+    review = Review.objects.get(pk=review_pk)
+    context = {
+        'review': review,
+    }
+    return render(request, 'reviews/detail.html', context)
