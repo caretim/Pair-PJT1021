47a391b7e30aeb3b3e63f00b13cd693bf73e224c
avocad06 <miggo2704@gmail.com>
2022-10-21 13:25:06 +0900

    function: reviews-update&delete

--- reviews/urls.py
+++ reviews/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("create/", views.create, name="create"),
     path("index/", views.index, name="index"),
     path("<int:review_pk>", views.detail, name="detail"),
+    path("<int:review_pk>/update/", views.update, name="update"),
+    path("<int:review_pk>/delete/", views.delete, name="delete"),
 ]
